12d4f98a800fc9abdc66f13deefa32b480ba270a
Rob Rohan <rob@therohans.com>
2024-05-26 14:28:22 +1200

    Mouse and basic drawing

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ```
 make build
 ```
+
+- https://libfpl.org/
+- https://miniaud.io/
--- src/main.c
+++ src/main.c
@@ -1,40 +1,80 @@
 #define FPL_IMPLEMENTATION
 #include "final_platform_layer.h"
 
+#define R2_MATHS_IMPLEMENTATION
+#include "r2_maths.h"
+
+struct InputState
+{
+    vec2 mousePos;
+    fplButtonState mouseStates[fplMouseButtonType_MaxCount];
+    float mouseWheelDelta;
+    uint64_t lastMouseWheelUpdateTime;
+};
+
 int main(int argc, char **args)
 {
     // Create default settings
     fplSettings settings = fplMakeDefaultSettings();
 
-    // Overwrite the video backend
-    // settings.video.backend = fplVideoBackendType_OpenGL;
-
-    // Legacy OpenGL
-    // settings.video.graphics.opengl.compabilityFlags = fplOpenGLCompabilityFlags_Legacy;
-    // or
-    // Modern OpenGL
-    // settings.video.graphics.opengl.compabilityFlags = fplOpenGLCompabilityFlags_Core;
-    // settings.video.graphics.opengl.majorVersion = 3;
-    // settings.video.graphics.opengl.minorVersion = 3;
+    settings.video.backend = fplVideoBackendType_Software;
+    settings.video.isAutoSize = true;
 
     if (fplPlatformInit(fplInitFlags_Video, &settings))
     {
 
-        // Event/Main loop
+        struct InputState input = {};
         while (fplWindowUpdate())
         {
-            // Poll events
+            // Input
             fplEvent ev;
             while (fplPollEvent(&ev))
             {
-                /// ...
-                if (ev.keyboard.keyCode >= 32 && ev.keyboard.keyCode < 255)
+                switch (ev.type)
                 {
-                    printf("%li\n", ev.keyboard.keyCode);
+                case fplEventType_Mouse: {
+                    if (ev.mouse.type == fplMouseEventType_Move)
+                    {
+                        input.mousePos.x = (float)ev.mouse.mouseX;
+                        input.mousePos.y = (float)ev.mouse.mouseY;
+                    }
+                    else if (ev.mouse.type == fplMouseEventType_Button)
+                    {
+                         input.mouseStates[(int)ev.mouse.mouseButton] = ev.mouse.buttonState;
+                    }
+                    else if (ev.mouse.type == fplMouseEventType_Wheel)
+                    {
+                        input.mouseWheelDelta = ev.mouse.wheelDelta;
+                        input.lastMouseWheelUpdateTime = fplMillisecondsQuery();
+                    }
+                    printf("%d, %d :: %d %d %d\n", 
+                        (int)input.mousePos.x, (int)input.mousePos.y,
+                        input.mouseStates[0], input.mouseStates[1], input.mouseStates[2]
+                    );
+                }
+                break;
+
+                case fplEventType_Keyboard: {
+                    if (ev.keyboard.keyCode >= 32 && ev.keyboard.keyCode < 255)
+                    {
+                        printf("%li\n", ev.keyboard.keyCode);
+                    }
+                }
+                break;
                 }
             }
 
-            // your code goes here
+            // Render
+            fplVideoBackBuffer *buff = fplGetVideoBackBuffer();
+            for (uint32_t y = 0; y < buff->height; ++y)
+            {
+                uint32_t *p = (uint32_t *)((uint8_t *)buff->pixels + y * buff->lineWidth);
+                for (uint32_t x = 0; x < buff->width; ++x)
+                {
+                    uint32_t color = 0xFFFFFF00;
+                    *p++ = color;
+                }
+            }
 
             fplVideoFlip();
         }
