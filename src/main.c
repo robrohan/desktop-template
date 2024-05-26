991012929dfc3de4547d25093da209387139b5a3
Rob Rohan <rob@therohans.com>
2024-05-26 12:45:29 +1200

    Adding library licenses

--- src/main.c
+++ src/main.c
@@ -1,45 +1,51 @@
 #define FPL_IMPLEMENTATION
 #include "final_platform_layer.h"
 
-int main(int argc, char **args) {
-  // Create default settings
-  fplSettings settings = fplMakeDefaultSettings();
+int main(int argc, char **args)
+{
+    // Create default settings
+    fplSettings settings = fplMakeDefaultSettings();
 
-  // Overwrite the video backend
-  settings.video.backend = fplVideoBackendType_OpenGL;
+    // Overwrite the video backend
+    settings.video.backend = fplVideoBackendType_OpenGL;
 
-  
-  // Legacy OpenGL
-  // settings.video.graphics.opengl.compabilityFlags =
-  // fplOpenGLCompabilityFlags_Legacy;
+    // Legacy OpenGL
+    // settings.video.graphics.opengl.compabilityFlags =
+    // fplOpenGLCompabilityFlags_Legacy;
 
-  // or
+    // or
 
-  // Modern OpenGL
-  settings.video.graphics.opengl.compabilityFlags = fplOpenGLCompabilityFlags_Core;
-  settings.video.graphics.opengl.majorVersion = 3;
-  settings.video.graphics.opengl.minorVersion = 3;
+    // Modern OpenGL
+    settings.video.graphics.opengl.compabilityFlags = fplOpenGLCompabilityFlags_Core;
+    settings.video.graphics.opengl.majorVersion = 3;
+    settings.video.graphics.opengl.minorVersion = 3;
 
-  if (fplPlatformInit(fplInitFlags_Video, &settings)) {
-    
-    // Event/Main loop
-    while (fplWindowUpdate()) {
-      // Poll events
-      fplEvent ev;
-      while (fplPollEvent(&ev)) {
-        /// ...
-        if(ev.keyboard.keyCode >= 32 && ev.keyboard.keyCode < 255) {
-        	printf("%li\n", ev.keyboard.keyCode);
-       	}
-      }
+    if (fplPlatformInit(fplInitFlags_Video, &settings))
+    {
 
-      // your code goes here
+        // Event/Main loop
+        while (fplWindowUpdate())
+        {
+            // Poll events
+            fplEvent ev;
+            while (fplPollEvent(&ev))
+            {
+                /// ...
+                if (ev.keyboard.keyCode >= 32 && ev.keyboard.keyCode < 255)
+                {
+                    printf("%li\n", ev.keyboard.keyCode);
+                }
+            }
 
-      fplVideoFlip();
+            // your code goes here
+
+            fplVideoFlip();
+        }
+        fplPlatformRelease();
+        return 0;
     }
-    fplPlatformRelease();
-    return 0;
-  } else {
-    return -1;
-  }
+    else
+    {
+        return -1;
+    }
 }
