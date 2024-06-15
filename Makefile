8dec2a2848b8679157daa14fdf18df438c23077c
Rob Rohan <rob@therohans.com>
2024-06-16 09:06:38 +1200

    Trying RGFW and cross platform build

--- Makefile
+++ Makefile
@@ -2,13 +2,50 @@
 
 CC=gcc
 APP=example
-PLATFORM=linux
+PLATFORM=Linux
 
 C_ERRS += -Wall -Wextra -Wpedantic \
 		-Wformat=2 -Wno-unused-parameter -Wshadow \
 		-Wwrite-strings -Wstrict-prototypes -Wold-style-definition \
 		-Wredundant-decls -Wnested-externs -Wmissing-include-dirs \
 		-Wno-unused
+
+DEBUG = -ggdb
+STATIC =
+# Windows mignw32 needs static
+ifeq ($(CC),x86_64-w64-mingw32-gcc)
+	STATIC = --static
+endif
+
+ifeq ($(PLATFORM),Windows)
+	LIBS := -ggdb \
+		-lshell32 \
+		-lwinmm \
+		-lgdi32 \
+		-lopengl32 \
+		$(STATIC)
+	EXT = .exe
+endif
+ifeq ($(PLATFORM),Darwin)
+	LIBS := -lm \
+		-framework Foundation \
+		-framework AppKit \
+		-framework OpenGL \
+		-framework CoreVideo \
+		$(STATIC)
+	EXT = 
+endif
+ifeq ($(PLATFORM),Linux)
+	LIBS := -lXrandr \
+		-lX11 \
+		-lm \
+		-lGL \
+		-ldl \
+		-lpthread \
+		$(STATIC)
+	EXT =
+endif
+
 
 dummy:
 	@echo "Probably want: make build"
@@ -17,18 +54,13 @@ clean:
 	rm -f ./libs/*.a
 	rm -rf ./build
 
-CFLAGS += `pkg-config --cflags x11 xext`
-LIBS += `pkg-config --libs x11 xext`
-
-build: build_linux
-
 # Do the actual build
-build_linux:
+build:
 	mkdir -p ./build/$(PLATFORM)
-	$(CC) $(C_ERRS) $(CFLAGS) -std=c11 \
+	$(CC) $(CUSTOM_CFLAGS) $(C_ERRS) $(CFLAGS) $(DEBUG) -std=c99 \
 		vendor/wefx/wefx.c \
 		src/main.c \
 		-I./vendor/ \
 		-I./vendor/wefx \
-		-o build/$(PLATFORM)/$(APP) -lm -fopenmp \
-		$(LIBS) -lXi -lXcursor -lEGL -lGL
+		-o build/$(PLATFORM)/$(APP)$(EXT) \
+		$(LIBS)
--- src/main.c
+++ src/main.c
@@ -1,37 +1,107 @@
-// #define SOKOL_NOAPI
-#define SOKOL_GLCORE
-// #define SOKOL_GLES3
-#define SOKOL_APP_IMPL
-#include "sokol_app.h"
+#define RGFW_IMPLEMENTATION
+#define RGFW_BUFFER
+#include "RGFW.h"
 
+u8 icon[4 * 3 * 3] = {0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF,
+                      0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF,
+                      0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF};
 
-void frame_cb(void)
+RGFW_area screenSize;
+
+/* fill buffer with a color, clearing anything that was on it */
+void clear(RGFW_window *win, u8 color[3])
 {
-    // called 60fps frame render
-    // printf("hi\n");
+    /* if all the values are the same */
+    if (color[0] == color[1] && color[0] == color[2])
+    {
+        /* set it all in one function */
+        memset(win->buffer, 0, screenSize.w * win->r.h * 4 * sizeof(u8));
+        return;
+    }
+
+    /* else we'll have to something more complex... */
+
+    /* loop through each *pixel* (not channel) of the buffer */
+    u32 x, y;
+    for (y = 0; y < (u32)win->r.h; y++)
+    {
+        for (x = 0; x < screenSize.w; x++)
+        {
+            u32 index = (y * 4 * screenSize.w) + x * 4;
+
+            /* copy the color to that pixel */
+            memcpy(win->buffer + index, color, 3 * sizeof(u8));
+        }
+    }
 }
 
-void event_cb(const sapp_event *event)
-{
-    // called when an event happens
-    // printf("howdy %i\n", event);
-}
+// /* this can also be used to convert BGR to RGB */
+// void bitmap_rgbToBgr(u8 *bitmap, RGFW_area a)
+// {
+//     /* loop through each *pixel* (not channel) of the buffer */
+//     u32 x, y;
+//     for (y = 0; y < a.h; y++)
+//     {
+//         for (x = 0; x < a.w; x++)
+//         {
+//             u32 index = (y * 4 * a.w) + x * 4;
 
-void init_cb(void)
-{
-    // called after the 3d context is made
-    printf("%ix%i\n", sapp_width(), sapp_height());
-    printf("GL Version: %i.%i\n", 
-        sapp_gl_get_major_version(), sapp_gl_get_minor_version());
-}
+//             u8 red = bitmap[index];
+//             bitmap[index] = bitmap[index + 2];
+//             bitmap[index + 2] = red;
+//         }
+//     }
+// }
 
-sapp_desc sokol_main(int argc, char* argv[]) {
-    return (sapp_desc) {
-        .width = 640,
-        .height = 480,
-        .init_cb = init_cb,
-        .frame_cb = frame_cb,
-        // .cleanup_cb = my_cleanup_func,
-        .event_cb = event_cb,
-    };
+// void drawBitmap(RGFW_window *win, u8 *bitmap, RGFW_rect rect)
+// {
+//     u32 y;
+//     for (y = 0; y < (u32)rect.h; y++)
+//     {
+//         u32 index = (rect.y + y) * (4 * screenSize.w) + rect.x * 4;
+//         memcpy(win->buffer + index, bitmap + (4 * rect.w * y), rect.w * 4 * sizeof(u8));
+//     }
+// }
+
+// void drawRect(RGFW_window *win, RGFW_rect r, u8 color[3])
+// {
+//     for (int x = r.x; x < (r.x + r.w); x++)
+//     {
+//         for (int y = r.y; y < (r.y + r.h); y++)
+//         {
+//             int index = y * (4 * screenSize.w) + x * 4;
+
+//             memcpy(win->buffer + index, color, 3 * sizeof(u8));
+//         }
+//     }
+// }
+
+int main(void)
+{
+    RGFW_window *win = RGFW_createWindow("Basic buffer example", RGFW_RECT(0, 0, 500, 500), RGFW_CENTER);
+    win->fpsCap = 60;
+
+    screenSize = RGFW_getScreenSize();
+
+    i8 running = 1;
+    while (running)
+    {
+        while (RGFW_window_checkEvent(win))
+        {
+            if (win->event.type == RGFW_quit || RGFW_isPressedI(win, RGFW_Escape))
+            {
+                running = 0;
+                break;
+            }
+        }
+
+        clear(win, (u8[3]){0, 0, 255});
+        // drawRect(win, RGFW_RECT(200, 200, 200, 200), (u8[3]){255, 0, 0});
+        // drawBitmap(win, icon, RGFW_RECT(100, 100, 3, 3));
+
+        RGFW_window_setGPURender(win, 0);
+        RGFW_window_swapBuffers(win);
+    }
+
+    RGFW_window_close(win);
 }
