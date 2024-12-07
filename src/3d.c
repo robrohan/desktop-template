dc740a7802678285eb058931a4cd104c2fbe9d5c
Rob Rohan <rob@therohans.com>
2024-12-08 10:32:39 +1300

    Adding references

--- src/3d.c
+++ src/3d.c
@@ -1,3 +1,9 @@
+/*
+    * https://joshbeam.com/articles/triangle_rasterization/
+    * https://www.cs.drexel.edu/~deb39/Classes/Papers/comp175-06-pineda.pdf
+    * https://fgiesen.wordpress.com/2013/02/06/the-barycentric-conspirac/
+    * https://jtsorlinis.github.io/rendering-tutorial/
+ */
 #include "3d.h"
 
 #define PIXEL_SIZE 1
@@ -8,6 +14,21 @@ i32 edge_cross(vector *a, vector *b, vector *c)
     return (b->x - a->x) * (c->y - a->y) - (b->y - a->y) * (c->x - a->x);
 };
 
+
+/* Are we looking at a triangle like:
+    B    C
+    ----|
+    \   |
+     \  |
+      \ |
+       A
+  (to prevent edge overdraw)
+*/
+i8 is_top_left(vector *s, vector *e)
+{
+    vector edge = { e->x - s->x, e->y - s->y};
+    return (edge.y == 0 && edge.x > 0) || edge.y > 0;
+}
 
 /* Clockwise 
          B
@@ -26,12 +47,21 @@ void triangle_fill(vertex A, vertex B, vertex C)
          return;
     }
 
+    // This will squeeze everything towards the center of the triangle a bit 
+    // if it has a left/top bias - which will help with overlapping overdraw
+    i32 bias0 = is_top_left(&A.vec, &B.vec) ? 0 : -1;
+    i32 bias1 = is_top_left(&B.vec, &C.vec) ? 0 : -1;
+    i32 bias2 = is_top_left(&C.vec, &A.vec) ? 0 : -1;
+
     i32 minX = MIN(MIN(A.vec.x, B.vec.x), C.vec.x);
     i32 minY = MIN(MIN(A.vec.y, B.vec.y), C.vec.y);
     i32 maxX = MAX(MAX(A.vec.x, B.vec.x), C.vec.x);
     i32 maxY = MAX(MAX(A.vec.y, B.vec.y), C.vec.y);
 
-    // wefx_rect(minX, maxY, maxX, minY, 1);
+#ifdef DEBUG_BOX_TRIANGLE
+    wefx_color(0xff, 0xff, 0xff);
+    wefx_rect(minX, maxY, maxX, minY, 1);
+#endif
 
     vertex SP = (vertex){ {0, 0}, 0, 0};
     for (SP.vec.y = minY; SP.vec.y < maxY; SP.vec.y+=PIXEL_SIZE)
@@ -39,11 +69,10 @@ void triangle_fill(vertex A, vertex B, vertex C)
         // wefx_color(rand() % 0xff, rand() % 0xff, rand() & 0xff);
         for (SP.vec.x = minX; SP.vec.x < maxX; SP.vec.x+=PIXEL_SIZE)
         {
-            i32 AB_P = edge_cross(&A.vec, &B.vec, &SP.vec);
-            i32 BC_P = edge_cross(&B.vec, &C.vec, &SP.vec);
-            i32 CA_P = edge_cross(&C.vec, &A.vec, &SP.vec);
+            i32 AB_P = edge_cross(&A.vec, &B.vec, &SP.vec) + bias0;
+            i32 BC_P = edge_cross(&B.vec, &C.vec, &SP.vec) + bias1;
+            i32 CA_P = edge_cross(&C.vec, &A.vec, &SP.vec) + bias2;
             
-            // printf("%d %d %d\n", wA, wB, wC);
             // Our graphics system has 0,0 on the bottom left not the
             // top right
             if (AB_P <= 0 && BC_P <= 0 && CA_P <= 0)
@@ -54,6 +83,7 @@ void triangle_fill(vertex A, vertex B, vertex C)
                 i32 wA = (BC_P *100) / AB_C;
                 i32 wB = (CA_P *100) / AB_C;
                 i32 wC = (AB_P *100) / AB_C;
+                // printf("%d %d %d\n", wA, wB, wC);
 
                 ui8 r = (255.0 * wA) + (000.0 * wB) + (000.0 * wC);
                 ui8 g = (000.0 * wA) + (255.0 * wB) + (000.0 * wC);
@@ -65,12 +95,9 @@ void triangle_fill(vertex A, vertex B, vertex C)
     }
 }
 
-
-vertex tri[7];
-
 void draw_scene(i32 time, i32 W, i32 H)
 {
-
+    vertex tri[7];
     tri[0] = (vertex){ {100, 10} };
     tri[1] = (vertex){ {W >> 1, abs( (H) * (sin(time * .01)))} };
     tri[2] = (vertex){ {W - 100, 10} };
@@ -78,37 +105,13 @@ void draw_scene(i32 time, i32 W, i32 H)
     tri[3] = (vertex){ {W - 100, H-10} };
     tri[4] = (vertex){ {W - 10, H-50} };
 
-
-
-    // wefx_color(0xff, 0xff, 0xff);
-    // // only draw clockwise triangles
-    // i32 abc = edge_function(tri[0], tri[1], tri[2]);
-    // if (abc < 0)
-    // {
-    //     draw_triangle(tri);
-    // }
-
-    // https://jtsorlinis.github.io/rendering-tutorial/
-    // Check if the point is inside the edge AB
-    // if (ABP >= 0) {
-    // Point is inside the edge AB
-    // }
-
     wefx_clear();
 
     wefx_color(0xff, 0xff, 0xff);
     vertex P = (vertex){ {W >> 1, H >> 1, 1} };
-    // wefx_point(P.vec.x, P.vec.y);
     wefx_pixel(P.vec.x, P.vec.y, 4);
 
-    // Calculate our edge function for all three edges of the triangle ABC
-    // i32 AB_P = edge_function(A, B, P);
-    // i32 BC_P = edge_function(B, C, P);
-    // i32 CA_P = edge_function(C, A, P);
-    // if (AB_P < 0 && BC_P < 0 && CA_P < 0)
-    // {
-    //     printf("%d %d %d \n", AB_P, BC_P, CA_P);
-    // }
+
     triangle_fill(tri[0], tri[1], tri[2]);
     triangle_fill(tri[2], tri[3], tri[4]);
     triangle_fill(tri[3], tri[2], tri[1]);
